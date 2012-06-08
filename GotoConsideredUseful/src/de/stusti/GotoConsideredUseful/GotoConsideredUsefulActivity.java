719ed85703c446939d956763d277c133dfc78fd8
Lennart <lsobirey@physnet.uni-hamburg.de>
2012-06-08 20:01:58 +0200

    Mit kleinen Änderungen
    Signed-off-by: Lennart <lsobirey@physnet.uni-hamburg.de>

--- GotoConsideredUseful/src/de/stusti/GotoConsideredUseful/GotoConsideredUsefulActivity.java
+++ GotoConsideredUseful/src/de/stusti/GotoConsideredUseful/GotoConsideredUsefulActivity.java
@@ -9,5 +9,6 @@ public class GotoConsideredUsefulActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //Kommentar zum Testen des eGit
     }
 }
