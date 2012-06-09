cf2f5f3e32e464ef94e77613dfa1c7e38677bfd6
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2012-06-09 11:51:40 +0200

    added callGeo mathod to spawn intent

--- GotoConsideredUseful/src/de/stusti/GotoConsideredUseful/GotoConsideredUsefulActivity.java
+++ GotoConsideredUseful/src/de/stusti/GotoConsideredUseful/GotoConsideredUsefulActivity.java
@@ -1,13 +1,25 @@
 package de.stusti.GotoConsideredUseful;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GotoConsideredUsefulActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        TextView tv = new TextView(this);
+        tv.setText("Hello, Android");
+        setContentView(tv);
+        
+        callGeo("Berlin Hbf");
+    }
+    
+    protected void callGeo(String destination) {
+    	Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=" + destination));
+        startActivity(i);
     }
 }
