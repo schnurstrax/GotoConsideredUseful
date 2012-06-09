9215b374b403e482fe42b6da82428fcaa694dea4
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2012-06-09 23:41:24 +0200

    activating GPS on start

--- GotoConsideredUseful/src/de/stusti/GotoConsideredUseful/GotoConsideredUsefulActivity.java
+++ GotoConsideredUseful/src/de/stusti/GotoConsideredUseful/GotoConsideredUsefulActivity.java
@@ -5,7 +5,11 @@ import java.util.ArrayList;
 import java.util.Calendar;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -14,10 +18,13 @@ import android.widget.ListView;
 public class GotoConsideredUsefulActivity extends Activity {
 	
 	protected ListView listView;
+	protected LocationListener locationListener;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	  super.onCreate(savedInstanceState);
+	  	
+	  	activateGPS();
 
 	  	// Get events from calendar.
 		EventHandler eventHandler = new EventHandler(this);
@@ -41,6 +48,39 @@ public class GotoConsideredUsefulActivity extends Activity {
         listView.setAdapter(adap);
 	}
 	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		
+		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		
+		locationManager.removeUpdates(this.locationListener);
+	}
+	
+	protected void activateGPS() {
+		// Acquire a reference to the system Location Manager
+		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+		// Define a listener that responds to location updates
+		this.locationListener = new LocationListener() {
+		    public void onLocationChanged(Location location) {
+		      // Called when a new location is found by the network location provider.
+		      makeUseOfNewLocation(location);
+		    }
+
+		    private void makeUseOfNewLocation(Location location) {}
+
+			public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+		    public void onProviderEnabled(String provider) {}
+
+		    public void onProviderDisabled(String provider) {}
+		  };
+
+		// Register the listener with the Location Manager to receive location updates
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000, 0, this.locationListener);
+	}
+
 	protected void callGeo(String destination){
 		AddressSearcher addressSearcher = new AddressSearcher(this);
 		String query = destination;
