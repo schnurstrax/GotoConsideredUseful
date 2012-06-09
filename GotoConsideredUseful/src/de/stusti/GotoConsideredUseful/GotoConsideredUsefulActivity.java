e5e08d3c071fdd7c33401204ad64cfc71cd4b0d3
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2012-06-09 23:00:39 +0200

    extracted Geocoder methods to AddressSearcher.java

--- GotoConsideredUseful/src/de/stusti/GotoConsideredUseful/GotoConsideredUsefulActivity.java
+++ GotoConsideredUseful/src/de/stusti/GotoConsideredUseful/GotoConsideredUsefulActivity.java
@@ -8,9 +8,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
 public class GotoConsideredUsefulActivity extends Activity {
 	
