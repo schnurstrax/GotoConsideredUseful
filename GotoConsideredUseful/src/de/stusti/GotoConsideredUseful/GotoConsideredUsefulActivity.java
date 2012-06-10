9b4d160e51d99e85b75cf7d34bca54a7d81851e3
Lennart <lsobirey@physnet.uni-hamburg.de>
2012-06-10 11:01:52 +0200

    asf
    Signed-off-by: Lennart <lsobirey@physnet.uni-hamburg.de>

--- GotoConsideredUseful/src/de/stusti/GotoConsideredUseful/GotoConsideredUsefulActivity.java
+++ GotoConsideredUseful/src/de/stusti/GotoConsideredUseful/GotoConsideredUsefulActivity.java
@@ -9,12 +9,13 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 public class GotoConsideredUsefulActivity extends Activity {
 	
 	protected ListView listView;
-	
+	GotoConsideredUsefulActivity ggg = this;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	  super.onCreate(savedInstanceState);
@@ -37,8 +38,25 @@ public class GotoConsideredUsefulActivity extends Activity {
 		adapterThingy adap = new adapterThingy(this, R.layout.termin_listitem, eventArray);
 		listView = (ListView)findViewById(R.id.listView1);       
 		View header = (View)getLayoutInflater().inflate(R.layout.listview_header_row, null);
+		ImageView b = (ImageView)header.findViewById(R.id.settings);
+		b.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent intent = new Intent(ggg, chooseCalendar.class);
+				intent.putExtra("calendars", new String[]{"",""});//TODO String Array Reein
+				startActivityForResult(intent,1);
+				
+			}
+		});
         listView.addHeaderView(header);    
         listView.setAdapter(adap);
+	}
+	
+	protected void onActivityResult(int i, int res, Intent asd) {
+		Integer ress = new Integer(res);
+		//boolean[] nums = new boolean[ress.bitCount()];
+		// TODO den boolean-array abnehmen
+		
 	}
 	
 	protected void callGeo(String destination){
