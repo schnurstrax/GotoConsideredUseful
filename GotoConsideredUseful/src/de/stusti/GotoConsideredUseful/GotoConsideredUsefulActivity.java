d6ff967ae6980fed1f6591520bb784b8d75c9e7d
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2012-06-10 13:59:51 +0200

    reverted GotoConsideredUseful

--- GotoConsideredUseful/src/de/stusti/GotoConsideredUseful/GotoConsideredUsefulActivity.java
+++ GotoConsideredUseful/src/de/stusti/GotoConsideredUseful/GotoConsideredUsefulActivity.java
@@ -1,42 +1,151 @@
 package de.stusti.GotoConsideredUseful;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.LinkedList;
-import java.util.List;
-
-import de.stusti.GotoConsideredUseful.calendars.Event;
-import de.stusti.GotoConsideredUseful.calendars.EventHandler;
-import de.stusti.GotoConsideredUseful.contacts.ContactHandler;
-import de.stusti.GotoConsideredUseful.location.AddressSearcher;
-import de.stusti.GotoConsideredUseful.location.MapViewActivity;
-
 import android.app.Activity;
-import android.app.AlertDialog.Builder;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.DialogInterface.OnClickListener;
-import android.content.DialogInterface.OnMultiChoiceClickListener;
-import android.database.Cursor;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.net.Uri;
-import android.os.Bundle;
-import android.util.Pair;
-import android.view.KeyEvent;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.Toast;
 
 public class GotoConsideredUsefulActivity extends Activity {
+
+//	protected ListView listView;
+//	protected LocationListener locationListener;
+//	
+//	GotoConsideredUsefulActivity ggg = this;
+//	@Override
+//	public void onCreate(Bundle savedInstanceState) {
+//	  super.onCreate(savedInstanceState);
+//	  	
+//	  	activateGPS();
+//
+//	  	// Get events from calendar.
+//		EventHandler eventHandler = new EventHandler(this);
+//		Calendar calendar = Calendar.getInstance();		
+//		
+//		long today = calendar.getTimeInMillis();
+//		int hours = 48;
+//		ArrayList<Event> events = eventHandler.getEvents(today, hours);
+//		//ArrayList<Event> eventsWithLocation = eventHandler.getEventsWithLocation(today, hours);		
+//		
+//		// Try to add locations using contact information.
+//		ContactHandler contactHandler = new ContactHandler(this);
+//		contactHandler.addLocationsToEvents(events);
+//		
+//		// Convert ArrayList to Event-Array.
+//		Event []eventArray = new Event[events.size()];
+//		events.toArray(eventArray);
+//		
+//		// Add events to the listView.
+//	  	setContentView(R.layout.main2);	  	
+//		adapterThingy adap = new adapterThingy(this, R.layout.termin_listitem, eventArray);
+//		listView = (ListView)findViewById(R.id.listView1);       
+//		View header = (View)getLayoutInflater().inflate(R.layout.listview_header_row, null);
+//		ImageView b = (ImageView)header.findViewById(R.id.settings);
+////		b.setOnClickListener(new View.OnClickListener() {
+////			
+//////			public void onClick(View v) {
+//////				Intent intent = new Intent(ggg, chooseCalendar.class);
+//////				intent.putExtra("calendars", new String[]{"",""});//TODO String Array Reein
+//////				startActivityForResult(intent,1);
+//////				
+//////			}
+////		});
+//        listView.addHeaderView(header);    
+//        listView.setAdapter(adap);
+//	}
+//	
+//	@Override
+//	protected void onPause() {
+//		super.onPause();
+//		//remove gps 
+//		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+//		locationManager.removeUpdates(this.locationListener);
+//	}
+//	
+//	protected void activateGPS() {
+//		// Acquire a reference to the system Location Manager
+//		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+//
+//		// Define a listener that responds to location updates
+//		this.locationListener = new LocationListener() {
+//		    public void onLocationChanged(Location location) {
+//		      // Called when a new location is found by the network location provider.
+//		      makeUseOfNewLocation(location);
+//		    }
+//
+//		    private void makeUseOfNewLocation(Location location) {}
+//
+//			public void onStatusChanged(String provider, int status, Bundle extras) {}
+//
+//		    public void onProviderEnabled(String provider) {}
+//
+//		    public void onProviderDisabled(String provider) {}
+//		  };
+//
+//		// Register the listener with the Location Manager to receive location updates
+//		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000, 0, this.locationListener);
+//	}
+//
+//	protected void onActivityResult(int i, int res, Intent asd) {
+//		Integer ress = new Integer(res);
+//		//boolean[] nums = new boolean[ress.bitCount()];
+//		// TODO den boolean-array abnehmen
+//		
+//	}
+//	
+//	protected void callGeo(String destination){
+//		AddressSearcher addressSearcher = new AddressSearcher(this);
+//		String query = destination;
+//		
+//		try {
+//			query = addressSearcher.getCountryExtendedQuery(destination);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//    	Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=" + query));
+//        startActivity(i);
+//    }
+//	
+//	protected void callMap(String destination) {
+//		Intent intent = new Intent(this, MapViewActivity.class);
+//		Bundle bundle = new Bundle();
+//		bundle.putString("marker", destination);
+//		intent.putExtras(bundle);
+//		startActivity(intent);
+//	}
+
+//	protected ListView listView;
+//	protected LocationListener locationListener;
+//	@Override
+//	public void onCreate(Bundle savedInstanceState) {
+//	    super.onCreate(savedInstanceState);
+//	    setContentView(R.layout.asdfxml);
+//	    activateGPS();
+//
+//	  	// Get events from calendar.
+//		EventHandler eventHandler = new EventHandler(this);
+//		Calendar calendar = Calendar.getInstance();		
+//		
+//		long today = calendar.getTimeInMillis();
+//		int hours = 548;
+//		ArrayList<Event> events = eventHandler.getEvents(today, hours);
+//		//ArrayList<Event> eventsWithLocation = eventHandler.getEventsWithLocation(today, hours);		
+//		
+//		// Try to add locations using contact information.
+//		ContactHandler contactHandler = new ContactHandler(this);
+//		contactHandler.addLocationsToEvents(events);
+//		
+//		// Convert ArrayList to Event-Array.
+//		Event []eventArray = new Event[events.size()];
+//		events.toArray(eventArray);
+//	    GridView gridview = (GridView) findViewById(R.id.gridview);
+//	    gridview.setAdapter(new newAdapter(this, events));
+//	}
+//	
+//	
+//	
 	
-	private List<String> selectedCalendars = new LinkedList<String>();
 	
-	protected ListView listView;
+	
+	/*protected ListView listView;
 	protected LocationListener locationListener;
 	
 	GotoConsideredUsefulActivity ggg = this;
@@ -51,7 +160,7 @@ public class GotoConsideredUsefulActivity extends Activity {
 		Calendar calendar = Calendar.getInstance();		
 		
 		long today = calendar.getTimeInMillis();
-		int hours = 548;
+		int hours = 48;
 		ArrayList<Event> events = eventHandler.getEvents(today, hours);
 		//ArrayList<Event> eventsWithLocation = eventHandler.getEventsWithLocation(today, hours);		
 		
@@ -81,121 +190,67 @@ public class GotoConsideredUsefulActivity extends Activity {
         listView.addHeaderView(header);    
         listView.setAdapter(adap);
 	}
-	
-	public boolean onKeyUp(int keyCode, KeyEvent event) {
-	    if (keyCode == KeyEvent.KEYCODE_MENU) {
-	    	final Builder build = new Builder(this);
-		    build.setTitle("List selection");
-		    build.setCancelable(true);
-		    List<String> calendars = getCalendars();
-		    final String[] strings = calendars.toArray(new String [calendars.size()]);
-		    final OnMultiChoiceClickListener onClick =
-		      new OnMultiChoiceClickListener() {
-		        public void onClick(final DialogInterface dialog,
-		                                      final int which, final boolean isChecked) {
-
-		          if (isChecked) {
-		        	  if (!(selectedCalendars.contains(strings[which]))) {
-		        		  selectedCalendars.add(strings[which]);
-		        	  }
-		          } else {
-		        	 selectedCalendars.remove(strings[which]);
-		          }
-
-		        }
-		      };
-		    build.setMultiChoiceItems(strings, null, onClick);
-		    build.setPositiveButton("Done", new OnClickListener() {
-		      public void onClick(final DialogInterface dialog,
-		                                    final int which) {
-		        String message = null;
-
-		        message = "You selected '" + selectedCalendars.size() + "'";
-		        Toast.makeText(GotoConsideredUsefulActivity.this, message, Toast.LENGTH_LONG).show();
-		      }
-		    });
-		    build.show();
-	    }
-	    return true;
-	}
-	
-	protected List<String> getCalendars() {
-	      
-		  Cursor cursor = this.getContentResolver().query(Uri.parse("content://com.android.calendar/calendars"),
-		          (new String[] {"displayName"}), null, null, null);
-		  List<String> calendars = new LinkedList<String>();
-		  if(cursor.getCount() != 0){				
-			  cursor.moveToFirst();		
-			
-			  while (cursor.moveToNext()) {
-				  calendars.add(cursor.getString(0));
-			  }
-				
-			  cursor.close();
-			  return calendars;
-		  }
-		  return null;
-    }
-	
-	@Override
-	protected void onPause() {
-		super.onPause();
-		//remove gps 
-		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-		locationManager.removeUpdates(this.locationListener);
-	}
-	
-	protected void activateGPS() {
-		// Acquire a reference to the system Location Manager
-		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-
-		// Define a listener that responds to location updates
-		this.locationListener = new LocationListener() {
-		    public void onLocationChanged(Location location) {
-		      // Called when a new location is found by the network location provider.
-		      makeUseOfNewLocation(location);
-		    }
-
-		    private void makeUseOfNewLocation(Location location) {}
-
-			public void onStatusChanged(String provider, int status, Bundle extras) {}
-
-		    public void onProviderEnabled(String provider) {}
-
-		    public void onProviderDisabled(String provider) {}
-		  };
-
-		// Register the listener with the Location Manager to receive location updates
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000, 0, this.locationListener);
-	}
-
-	protected void onActivityResult(int i, int res, Intent asd) {
-		Integer ress = new Integer(res);
-		//boolean[] nums = new boolean[ress.bitCount()];
-		// TODO den boolean-array abnehmen
-		
-	}
-	
-	protected void callGeo(String destination){
-		AddressSearcher addressSearcher = new AddressSearcher(this);
-		String query = destination;
-		
-		try {
-			query = addressSearcher.getCountryExtendedQuery(destination);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-    	Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=" + query));
-        startActivity(i);
-    }
-	
-	protected void callMap(String destination) {
-		Intent intent = new Intent(this, MapViewActivity.class);
-		Bundle bundle = new Bundle();
-		bundle.putString("marker", destination);
-		intent.putExtras(bundle);
-		startActivity(intent);
-	}
+//	*/
+//	@Override
+//	protected void onPause() {
+//		super.onPause();
+//		//remove gps 
+//		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+//		locationManager.removeUpdates(this.locationListener);
+//	}
+//	
+//	protected void activateGPS() {
+//		// Acquire a reference to the system Location Manager
+//		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+//
+//		// Define a listener that responds to location updates
+//		this.locationListener = new LocationListener() {
+//		    public void onLocationChanged(Location location) {
+//		      // Called when a new location is found by the network location provider.
+//		      makeUseOfNewLocation(location);
+//		    }
+//
+//		    private void makeUseOfNewLocation(Location location) {}
+//
+//			public void onStatusChanged(String provider, int status, Bundle extras) {}
+//
+//		    public void onProviderEnabled(String provider) {}
+//
+//		    public void onProviderDisabled(String provider) {}
+//		  };
+//
+//		// Register the listener with the Location Manager to receive location updates
+//		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000, 0, this.locationListener);
+//	}
+//	
+//
+//	protected void onActivityResult(int i, int res, Intent asd) {
+//		Integer ress = new Integer(res);
+//		//boolean[] nums = new boolean[ress.bitCount()];
+//		// TODO den boolean-array abnehmen
+//		
+//	}
+//	
+//	protected void callGeo(String destination){
+//		AddressSearcher addressSearcher = new AddressSearcher(this);
+//		String query = destination;
+//		
+//		try {
+//			query = addressSearcher.getCountryExtendedQuery(destination);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//    	Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=" + query));
+//        startActivity(i);
+//    }
+//	
+//	protected void callMap(String destination) {
+//		Intent intent = new Intent(this, MapViewActivity.class);
+//		Bundle bundle = new Bundle();
+//		bundle.putString("marker", destination);
+//		intent.putExtras(bundle);
+//		startActivity(intent);
+//	}
 }
